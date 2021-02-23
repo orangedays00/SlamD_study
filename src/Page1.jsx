d0eb7a52a6a3bdc6a85209aba866b289503d327d
orangedays00 <44051980+orangedays00@users.noreply.github.com>
2021-02-23 23:25:05 +0900

    Initial commit

---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Page1 = () => {
+  return <div>HelloPage1!!</div>;
+};
+export default Page1;
---
+++ src/Shouhoku.jsx
@@ -0,0 +1,53 @@
+import "./styles.css";
+import { FC } from "react";
+import CharacterList, { Character } from "./CharacterList";
+import Img from "./og.jpg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Page1 from "./Page1";
+import { Link } from "react-router-dom";
+
+const Shouhoku: FC = () => {
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: "桜木花道",
+      grade: 1,
+      height: 189.2
+    },
+    {
+      id: 2,
+      name: "流川楓",
+      grade: 1,
+      height: 187.0
+    },
+    {
+      id: 3,
+      name: "宮城リョータ",
+      grade: 2,
+      height: 168.0
+    },
+    {
+      id: 4,
+      name: "三井寿",
+      grade: 3,
+      height: 184.0
+    },
+    {
+      id: 5,
+      name: "赤城剛憲",
+      grade: 3,
+      height: 197.0
+    }
+  ];
+
+  return (
+    <div className="container">
+      <header>
+        <h1>SLAM DUNK 登場人物</h1>
+      </header>
+      <CharacterList school="湘北高校" characters={characters} />
+    </div>
+  );
+};
+
+export default Shouhoku;
